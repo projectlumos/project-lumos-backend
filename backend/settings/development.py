1a24b0af6dae5f44d74f1a273f34d796a8b1b619
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-02-11 20:46:21 +0530

    set defaults for postgres and secret key

--- backend/settings/development.py
+++ backend/settings/development.py
@@ -5,9 +5,9 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_env_variable('DATABASE_NAME'),
-        'USER': get_env_variable('DATABASE_USER'),
-        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'NAME': 'backend_db',
+        'USER': 'paritoshkw1',
+        'PASSWORD': 'Parwad321',
         'HOST': '',
         'PORT': '',
     }
