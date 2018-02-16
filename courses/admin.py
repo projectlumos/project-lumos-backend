8ad4d409ffa4bd003748f9148a04a3eb724f317a
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-02-16 09:52:09 +0530

    added multi-language to list_display, changed raw_id_fields to autocomplete_fields

--- courses/admin.py
+++ courses/admin.py
@@ -7,6 +7,7 @@ class DomainModelAdmin(admin.ModelAdmin):
     list_display_links = ['domain_name','__str__']
     search_fields = ['domain_name','__str__']
     list_filter = ['domain_name']
+    readonly_fields = ['slug']
 
     class Meta:
         model = Domain
@@ -17,30 +18,31 @@ class LanguageModelAdmin(admin.ModelAdmin):
     list_display_links = ['language_name','__str__']
     search_fields = ['language_name','__str__']
     list_filter = ['language_name']
+    readonly_fields = ['slug']
 
     class Meta:
         model = Language
 
 
 class VideoModelAdmin(admin.ModelAdmin):
-    list_display = ['title', 'is_active', 'modified_at', 'created_at', 'skill_level']
+    list_display = ['title', 'is_active', 'modified_at', 'created_at', 'skill_level', 'multi_language']
     list_display_links = ['title']
-    list_filter = ['is_active', 'languages__language_name', 'domains__domain_name', 'skill_level', 'modified_at', 'created_at', 'multiple_languages']
+    list_filter = ['is_active', 'languages__language_name', 'domains__domain_name', 'skill_level', 'modified_at', 'created_at']
     search_fields = ['title', 'description', 'languages__language_name', 'domains__domain_name']
-    raw_id_fields = ['languages__language_name', 'domains__domain_name']   #should I use autocomplete_fields instead?
-    readonly_fields = ['multiple_languages']
+    autocomplete_fields = ['languages', 'domains']   #should I use autocomplete_fields instead?
+    readonly_fields = ['multiple_languages', 'slug']
 
     class Meta:
         model = Video
 
 
 class ExternalLinkModelAdmin(admin.ModelAdmin):
-    list_display = ['title', 'is_active', 'modified_at', 'created_at', 'skill_level']
+    list_display = ['title', 'is_active', 'modified_at', 'created_at', 'skill_level', 'multi_language']
     list_display_links = ['title']
-    list_filter = ['is_active', 'languages__language_name', 'domains__domain_name', 'skill_level', 'modified_at', 'created_at', 'multiple_languages']
+    list_filter = ['is_active', 'languages__language_name', 'domains__domain_name', 'skill_level', 'modified_at', 'created_at']
     search_fields = ['title', 'description', 'languages__language_name', 'domains__domain_name']
-    raw_id_fields = ['languages__language_name', 'domains__domain_name']
-    readonly_fields = ['multiple_languages']
+    autocomplete_fields = ['languages', 'domains']
+    readonly_fields = ['multiple_languages', 'slug']
 
     class Meta:
         model = ExternalLink
--- courses/models.py
+++ courses/models.py
@@ -1,50 +1,60 @@
 from django.db import models
-from utils.constants import SKILL_BEGINNER, SKILL_INTERMEDIATE, SKILL_ADVANCED, EXTERNAL_LINK_BLOG, EXTERNAL_LINK_TUTORIAL, EXTERNAL_LINK_COURSE
-from django.utils.text import slugify
+from courses.utils.constants import skill_levels, external_types
+from django.template.defaultfilters import slugify
+from django.db import IntegrityError
+from django.utils.crypto import get_random_string
 # Create your models here.
 
-class Language(models.Model):
-    language_name = models.CharField(max_length=20)
-    slug = models.SlugField(unique=True)
+def custom_slugify(source_field, suffix=False):
+    new_slug = slugify(source_field)
 
-    def _get_unique_slug(self):
-        slug = slugify(self.language_name)
-        unique_slug = slug
-        num = 1
-        while Language.objects.filter(slug=unique_slug).exists():
-            unique_slug = '{}-{}'.format(slug, num)
-            num += 1
-        return unique_slug
+    if suffix:
+        random_str = get_random_string(length=10)
+        new_slug = "{0}-{1}".format(new_slug, random_str)
+
+    return new_slug
+
+class Language(models.Model):
+    language_name = models.CharField(max_length=30)
+    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = self._get_unique_slug()
-        super(Language, self).save(*args, **kwargs)
+            self.slug = custom_slugify(source_field=self.language_name, suffix=False)
+
+        try:
+            super(Language, self).save(*args, **kwargs)
+
+        except IntegrityError:
+            self.slug = custom_slugify(source_field=self.language_name, suffix=True)
+            super(Language, self).save(*args, **kwargs)
 
     def __str__(self):
         return '{name}-{slug}'.format(name=self.language_name, slug=self.slug)
 
+    class Meta:
+        unique_together = ('language_name', 'slug')
 
 class Domain(models.Model):
-    domain_name = models.CharField(max_length=20)
+    domain_name = models.CharField(max_length=30)
     slug = models.SlugField(unique=True)
-
-    def _get_unique_slug(self):
-        slug = slugify(self.domain_name)
-        unique_slug = slug
-        num = 1
-        while Domain.objects.filter(slug=unique_slug).exists():
-            unique_slug = '{}-{}'.format(slug, num)
-            num += 1
-        return unique_slug
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = self._get_unique_slug()
-        super(Domain, self).save(*args, **kwargs)
+            self.slug = custom_slugify(source_field=self.domain_name, suffix=False)
+
+        try:
+            super(Domain, self).save(*args, **kwargs)
+
+        except IntegrityError:
+            self.slug = custom_slugify(source_field=self.domain_name, suffix=True)
+            super(Domain, self).save(*args, **kwargs)
 
     def __str__(self):
         return '{name}-{slug}'.format(name=self.domain_name, slug=self.slug)
+
+    class Meta:
+        unique_together = ('domain_name', 'slug')
 
 class RowInformation(models.Model):
     is_active = models.BooleanField(default=False)
@@ -56,36 +66,21 @@ class RowInformation(models.Model):
 
 
 class KnowledgeBase(RowInformation):
-    skill_levels = (
-        (SKILL_BEGINNER, 'BEGINNER'),
-        (SKILL_INTERMEDIATE, 'INTERMEDIATE'),
-        (SKILL_ADVANCED, 'ADVANCED')
-    )
-
     title = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(null=False, blank=True)    #if no description, store empty string
     slug = models.SlugField(unique=True)
-    languages = models.ManyToManyField(Language, related_name='knowledgebase_lang')
-    domains = models.ManyToManyField(Domain, related_name='knowledgebase_dom')
     skill_level = models.CharField(max_length=2, choices=skill_levels)
-
-    class Meta:
-        abstract = True
-        ordering = ['-modified_at','-created_at']
-
-    def _get_unique_slug(self):
-        slug = slugify(self.title)
-        unique_slug = slug
-        num = 1
-        while KnowledgeBase.objects.filter(slug=unique_slug).exists():
-            unique_slug = '{}-{}'.format(slug, num)
-            num += 1
-        return unique_slug
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = self._get_unique_slug()
-        super(KnowledgeBase, self).save(*args, **kwargs)
+            self.slug = custom_slugify(source_field=self.title, suffix=False)
+
+        try:
+            super(KnowledgeBase, self).save(*args, **kwargs)
+
+        except IntegrityError:
+            self.slug = custom_slugify(source_field=self.title, suffix=True)
+            super(KnowledgeBase, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.slug
@@ -95,23 +90,25 @@ class KnowledgeBase(RowInformation):
             return True
         else:
             return False
+    class Meta:
+        abstract = True
+        ordering = ['-modified_at','-created_at']
+        unique_together = ('title', 'slug')
 
-    multi_language.short_description = "resource has multiple languages or not"
+    multi_language.short_description = "multiple-languages"
     multiple_languages = property(multi_language)
+    multi_language.admin_order_field = '-modified_at'
 
 
 class Video(KnowledgeBase):
-    video_id = models.URLField(null=False, blank=False, unique=True)  #since video_id is a string
-
+    video_url = models.URLField(null=False, blank=False, unique=True)  #since video_id is a string
+    languages = models.ManyToManyField(Language, related_name='video_languages')
+    domains = models.ManyToManyField(Domain, related_name='video_domains')
 
 class ExternalLink(KnowledgeBase):
-    external_types = (     #indicate which type of external link
-        (EXTERNAL_LINK_BLOG, 'BLOG'),
-        (EXTERNAL_LINK_TUTORIAL, 'TUTORIAL'),
-        (EXTERNAL_LINK_COURSE, 'COURSE')
-    )
-
     link_url = models.URLField(null=False, blank=False, unique=True)
+    languages = models.ManyToManyField(Language, related_name='external_link_languages')
+    domains = models.ManyToManyField(Domain, related_name='external_link_domains')
     external_type = models.CharField(max_length=2, choices=external_types)
     paid = models.BooleanField(default=False)
 
