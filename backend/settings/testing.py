1fb641c00b06a615191c4001c94821fc068ac5b0
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-02-17 18:50:08 +0530

    changed settings and requirements.txt for heroku

--- backend/settings/testing.py
+++ backend/settings/testing.py
@@ -12,3 +12,7 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+import dj_database_url
+DATABASES[‘default’] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
