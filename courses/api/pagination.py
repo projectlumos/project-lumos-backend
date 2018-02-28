902e35b0d7274f0616862b25cf73346391221fc9
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-02-28 11:02:30 +0530

    changed max_page_size to 50

--- courses/api/pagination.py
+++ courses/api/pagination.py
@@ -6,10 +6,10 @@ from rest_framework.pagination import (
 class VideoPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     page_size = 15
-    max_page_size = 30
+    max_page_size = 50
 
 
 class ExternalLinkPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     page_size = 15
-    max_page_size = 30
+    max_page_size = 50
