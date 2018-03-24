dd7e1a5d80c56d83d510aa942a8a9a6f28802580
prashant <prashantpandey071@gmail.com>
2018-03-24 15:49:13 +0530

    updated queryset method returning only user specific information

---
+++ feedback/api/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsOwnerFeedback(BasePermission):
+    message = "You must be the owner of the object to view."
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            if obj.user == None:
+                # if user is anonymous then read-only
+                return True
+
+            # Check permissions for read-only request
+            elif obj.user == request.user:
+                return True
+        else:
+            # Check permissions for write request
+            if obj.user == request.user:
+                return False
+
